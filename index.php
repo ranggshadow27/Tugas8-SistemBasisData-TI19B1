09f8e86085644ddc8f0a905dff8ed7960614e542
rangga <46300525+ranggshadow27@users.noreply.github.com>
2021-06-08 10:17:54 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,95 +1,72 @@
 <?php
-include("koneksi.php");
-// query untuk menampilkan data
-$sql = 'SELECT * FROM mainboard';
-$result = mysqli_query($conn, $sql);
-$memory = 'SELECT * FROM memory';
-$memoryresult = mysqli_query($conn, $memory);
-$proc = 'SELECT * FROM processor';
-$procresult = mysqli_query($conn, $proc);
+include "koneksi.php";
+
+//query menampilkan data
+$databarang = "SELECT * FROM data_barang ORDER BY `data_barang`.`id_barang` ASC";
+$tipebarang = "SELECT * FROM tipe_barang";
+$ketbarang = "SELECT * FROM keterangan_barang";
+$printa = mysqli_query($conn, $databarang);
+$printb = mysqli_query($conn, $tipebarang);
+$printc = mysqli_query($conn, $ketbarang);
+
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
-    <head>
+<head>
     <meta charset="UTF-8">
-    <link href="style.css" rel="stylesheet" type="text/css" />
-    <title>Tugas 8 - Basis Data</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Koneksi Database</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
 </head>
 <body>
-    <div class="container">
-        <h1>Mainboard</h1>
-            <table>
-            <tr>
-                <th>Nama Barang</th>
-                <th>Chipset</th>
-                <th class="mini">Stok</th>
-                <th>Harga Barang</th>
-            </tr>
-            <?php if($result): ?>
-            <?php while($a = mysqli_fetch_array($result)): ?>
-            <tr>
-                <td><?= $a['nama_barang'];?></td>
-                <td><?= $a['chipset'];?></td>
-                <td><?= $a['stok'];?></td>
-                <td>Rp. <?= $a['harga_barang'];?></td>
-            </tr>
-            <?php endwhile; else: ?>
+    <div class="containers">
+        <h2>DATA BARANG</h2>
+        <hr><br>
+            <div class="main">
+                <table>
                 <tr>
-                    <td colspan="7">Belum ada data</td>
+                    <th>Nama Barang</th>
+                    <th>Kategori</th>
+                    <th>Keterangan</th>
+                    <th class="mini">Harga Barang</th>
+                    <th class="mini">Stok</th>
                 </tr>
-            <?php endif; ?>
-            </table>
-        <h1>Memory</h1>
-            <table>
-            <tr>
-                <th>Nama Barang</th>
-                <th class="mini">Tipe Memori</th>
-                <th class="mini">Speed</th>
-                <th class="mini">Kapasitas</th>
-                <th>Harga Barang</th>
-            </tr>
-            <?php if($memoryresult): ?>
-            <?php while($b = mysqli_fetch_array($memoryresult)): ?>
-            <tr>
-                <td><?= $b['nama_barang'];?></td>
-                <td><?= $b['tipe_memori'];?></td>
-                <td><?= $b['speed'];?></td>
-                <td><?= $b['kapasitas_memori'];?></td>
-                <td>Rp. <?= $b['harga_barang'];?></td>
-            </tr>
-
-            <?php endwhile; else: ?>
+                <?php while($a = mysqli_fetch_array($printa)): ?>
                 <tr>
-                    <td colspan="7">Belum ada data</td>
+                    <td><?= $a['nama_barang'];?></td>
+                    <td><?= $a['jenis_barang'];?></td>
+                    <td><?= $a['keterangan_barang'];?></td>
+                    <td>Rp. <?= $a['harga_barang'];?></td>
+                    <td><?= $a['stok_barang'];?></td>
                 </tr>
-            <?php endif; ?>
-            </table>
-        <h1>Prosesor</h1>
-            <table>
-            <tr>
-                <th>Nama Barang</th>
-                <th class="mini">Tipe Prosesor</th>
-                <th class="mini">Inti Prosesor</th>
-                <th class="mini">Clockspeed</th>
-                <th class="mini">TDP</th>
-                <th>Harga Barang</th>
-            </tr>
-            <?php if($procresult): ?>
-            <?php while($c = mysqli_fetch_array($procresult)): ?>
-            <tr>
-                <td><?= $c['nama_barang'];?></td>
-                <td><?= $c['tipe_processor'];?></td>
-                <td><?= $c['inti_processor'];?></td>
-                <td><?= $c['clockspeed'];?></td>
-                <td><?= $c['tdp'];?></td>
-                <td>Rp. <?= $c['harga_barang'];?></td>
-            </tr>
-            <?php endwhile; else: ?>
+                <?php endwhile;?>
+                 </table>
+            </div>
+        <br><hr><br>
+            <div class="mains">
+                <table class="tab">
                 <tr>
-                <td colspan="7">Belum ada data</td>
-            </tr>
-            <?php endif; ?>
-            </table>
+                    <th>Kategori Barang</th>
+                </tr>
+                <?php while($b = mysqli_fetch_array($printb)): ?>
+                <tr>
+                    <td><?= $b['tipe_barang'];?></td>
+                </tr>
+                <?php endwhile;?>
+                </table>
+                <table class="tab">
+                <tr>
+                    <th>Keterangan Barang</th>
+                </tr>
+                <?php while($c = mysqli_fetch_array($printc)): ?>
+                <tr>
+                    <td><?= $c['keterangan_barang'];?></td>
+                </tr>
+                <?php endwhile;?>
+                </table>
+            </div>
     </div>
 </body>
 </html>
--- koneksi.php
+++ koneksi.php
@@ -2,11 +2,6 @@
 $host = "localhost";
 $user = "root";
 $pass = "";
-$db = "basisdatapert10";
+$db = "tugas8_basisdata";
 $conn = mysqli_connect($host, $user, $pass, $db);
-if ($conn == false)
-{
-echo "Koneksi ke server gagal.";
-die();
-} #else echo "Koneksi berhasil";
 ?>
